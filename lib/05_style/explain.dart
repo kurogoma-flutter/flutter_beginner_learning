b6529e788c28e762cd41ba997f790dcc205510dc
Kurogoma939 <kan.sss.939@gmail.com>
2022-09-01 01:56:27 +0900

    Task5 base

---
+++ lib/05_style/explain.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Task5 extends StatelessWidget {
+  const Task5({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Task 5'),
+      ),
+      body: const Center(
+        child: Text('スタイル'),
+      ),
+    );
+  }
+}
